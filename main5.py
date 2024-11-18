88f68e375a06f880c743f1821e873a03e8bc392e
M-Irina <irina24mad@gmail.com>
2024-11-18 22:48:21 +0900

    Add files via upload

---
+++ main5.py
@@ -0,0 +1,13 @@
+name = "Irina"
+print(name, type(name))
+age = 60
+print(age, type(age))
+age = 15
+new_age = 17
+print(age + new_age)
+print (type(age+new_age))
+is_student ="True"
+print(is_student)
+
+
+
